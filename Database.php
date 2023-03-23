efa107e9658d7fcd55d3ac3d6b0031c5223841a0
Nick <nikolai.umrikhin@gmail.com>
2023-03-23 15:14:11 +0300

    master commit

--- Database.php
+++ Database.php
@@ -1,61 +1,73 @@
 <?php
 
-class DataBase {
-	
-	private $db;
+class DataBase
+{
 
-	public function __construct($file = 'config.ini'){
+    private $db;
 
-		if(!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
+    public function __construct($file = 'config.ini')
+    {
 
-		$dsn = 'mysql:host=' . $settings['database']['host'] . ';dbname=' . $settings['database']['db'] . ';charset=' . $settings['database']['charset'];
-		$opt = [
-			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-			PDO::ATTR_EMULATE_PREPARES   => false,
-		];
+        if (!$settings = parse_ini_file($file, true)) {
+            throw new exception('Unable to open ' . $file . '.');
+        }
 
-		$this->db = new PDO($dsn, $settings['database']['user'], $settings['database']['password'], $opt);
+        $dsn = 'mysql:host=' . $settings['database']['host'] . ';dbname=' . $settings['database']['db'] . ';charset=' . $settings['database']['charset'];
+        $opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
 
-	}
-	
-	public function query($sql, $params = []){
-		$stmt = $this->db->prepare($sql);
+        $this->db = new PDO($dsn, $settings['database']['user'], $settings['database']['password'], $opt);
 
-		if( !empty($params) ){
-			foreach($params as $key => $value){
-				$stmt->bindValue(":$key", $value);
-			}
-		}
+    }
 
-		$stmt->execute();
-		return $stmt->fetchAll();
-	}
+    public function query($sql, $params = [])
+    {
 
-	public function getAll($table, $sql = ''){
-		return $this->query("SELECT * FROM $table" . $sql);
-	}
+        $stmt = $this->db->prepare($sql);
 
-	public function getValue($value, $table, $sql = ''){
-		$result = $this->query("SELECT $value FROM $table WHERE $sql");
-		return $result[0];
-	}
+        if (!empty($params)) {
+            foreach ($params as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+        }
 
-	public function UserSearchRow($table, $telegram_id){
-		if($this->query("SELECT * FROM $table WHERE `telegram_id` LIKE $telegram_id")){
-			return true;
-		}
-		return false;
-	}
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 
-	public function InstertRow($params){
-		$this->query("INSERT INTO `telegram_users` (telegram_id, username, first_name, status) VALUES ( :telegram_id, :username, :first_name, :status )", $params);
-	}
+    public function getAll($table, $sql = '')
+    {
+        return $this->query("SELECT * FROM $table" . $sql);
+    }
 
+    public function getValue($value, $table, $sql = '')
+    {
+        $result = $this->query("SELECT $value FROM $table WHERE $sql");
+        return $result[0];
+    }
 
-	public function QuerryStatusUpdate($params){
-		$this->query('UPDATE `telegram_users` SET `username` = :username, `first_name` = :first_name, `status`= :status WHERE `telegram_id` = :telegram_id',  $params);
-	}
+    public function UserSearchRow($table, $telegram_id)
+    {
+        if ($this->query("SELECT * FROM $table WHERE `telegram_id` LIKE $telegram_id")) {
+            return true;
+        }
+        return false;
+    }
+
+    public function InstertRow($params)
+    {
+        $this->query("INSERT INTO `telegram_users` (telegram_id, username, first_name, status) VALUES ( :telegram_id, :username, :first_name, :status )",
+            $params);
+    }
+
+    public function QuerryStatusUpdate($params)
+    {
+        $this->query('UPDATE `telegram_users` SET `username` = :username, `first_name` = :first_name, `status`= :status WHERE `telegram_id` = :telegram_id',
+            $params);
+    }
 
 }
 
--- telegram_bot.php
+++ telegram_bot.php
@@ -3,100 +3,98 @@
 require_once('Controller.php');
 require_once('Database.php');
 
-$data = json_decode(file_get_contents('php://input'), TRUE);
+$data = json_decode(file_get_contents('php://input'), true);
 //create log file
-file_put_contents('log.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+file_put_contents('log.txt', '$data: ' . print_r($data, 1) . "\n", FILE_APPEND);
 
 define('TOKEN', 'TELEGRAM API KEY');
-$data   = $data['callback_query'] ? $data['callback_query'] : $data['message'];
-$method    = 'sendMessage';
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+$method = 'sendMessage';
 
 $params = [
-	'telegram_id' => $data['from']['id'],
-	'username' => $data['from']['username'],
-	'first_name' => $data['from']['first_name'],
-	'status' => NULL
+    'telegram_id' => $data['from']['id'],
+    'username' => $data['from']['username'],
+    'first_name' => $data['from']['first_name'],
+    'status' => null
 ];
 
 $controller = new MainController();
 $DataBase = new DataBase();
 
-$message  = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']), 'utf-8');
 
 $statusValue = $DataBase->getValue('`status`', 'telegram_users', "`telegram_id` = '" . $data['from']['id'] . "'");
 
-if($statusValue['status'] == 'CheckSKU'){
-	switch($message){
-		default:
-		$params['status'] = NULL;
-		$DataBase->QuerryStatusUpdate($params); 
-		$send_data['text'] = 'Ожидайте...';
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-		$prodInfo = $controller->getInfo($message);
-		$createCSV= $controller->createCSV($prodInfo, $data);
-		$controller->sendDocument($data['chat']['id'], $createCSV);
-		$send_data= ['text' => 'Отчет готов!'];
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-	}
-}
+if ($statusValue['status'] == 'CheckSKU') {
+    switch ($message) {
+        default:
+            $params['status'] = null;
+            $DataBase->QuerryStatusUpdate($params);
+            $send_data['text'] = 'Ожидайте...';
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+            $prodInfo = $controller->getInfo($message);
+            $createCSV = $controller->createCSV($prodInfo, $data);
+            $controller->sendDocument($data['chat']['id'], $createCSV);
+            $send_data = ['text' => 'Отчет готов!'];
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+    }
+} else {
+    switch ($message) {
+        case '/start':
+            if (!$DataBase->UserSearchRow('`telegram_users`', '\'' . $data['from']['id'] . '\'')) {
+                $DataBase->InstertRow($params);
+            }
+            $params['status'] = null;
+            $DataBase->QuerryStatusUpdate($params);
+            $send_data = [
 
-else{
-	switch($message){
-		case '/start':
-		if(!$DataBase->UserSearchRow('`telegram_users`', '\'' . $data['from']['id'] . '\'')){
-			$DataBase->InstertRow($params);
-		}
-		$params['status'] = NULL;
-		$DataBase->QuerryStatusUpdate($params);
-		$send_data = [
+                'text' => 'Приветствую! Данный бот поможет вам собрать информацию о товарах Wildberries.',
+                'reply_markup' => [
+                    'resize_keyboard' => true,
+                    'one_time_keyboard' => true,
+                    'keyboard' => [
+                        [
+                            ['text' => 'проверить артикулы'],
+                            ['text' => 'информация о боте'],
+                        ]
+                    ]
+                ]
+            ];
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+            break;
 
-			'text' => 'Приветствую! Данный бот поможет вам собрать информацию о товарах Wildberries.',
-			'reply_markup'  => [
-				'resize_keyboard' => true,
-				'one_time_keyboard'  => true,
-				'keyboard' => [
-					[
-						['text' => 'проверить артикулы'],
-						['text' => 'информация о боте'],
-					]
-				]
-			]
-		];
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-		break;
+        case 'проверить артикулы':
+            $params['status'] = 'CheckSKU';
+            $DataBase->QuerryStatusUpdate($params);
+            $send_data = ['text' => 'Введите артикулы товаров через запятую (Пример: 53840226,113251542 )'];
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+            break;
 
-		case 'проверить артикулы':
-		$params['status'] = 'CheckSKU';
-		$DataBase->QuerryStatusUpdate($params);
-		$send_data = ['text' => 'Введите артикулы товаров через запятую (Пример: 53840226,113251542 )'];
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-		break;
+        case 'информация о боте':
+            $send_data = ['text' => 'Данный бот позволяет получать информацию об артикулах товаров Wildberries. '];
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+            break;
 
-		case 'информация о боте':
-		$send_data = ['text' => 'Данный бот позволяет получать информацию об артикулах товаров Wildberries. '];
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-		break;
-
-		default:
-		$send_data = [
-			'text' => 'Команда не распознана.',
-			'reply_markup'  => [
-				'resize_keyboard' => true,
-				'keyboard' => [
-					[
-						['text' => 'проверить артикулы'],
-						['text' => 'информация о боте'],
-					]
-				]
-			]
-		];
-		$send_data['chat_id'] = $data['chat']['id'];
-		$controller->sendTelegram($method, $send_data);
-	}
+        default:
+            $send_data = [
+                'text' => 'Команда не распознана.',
+                'reply_markup' => [
+                    'resize_keyboard' => true,
+                    'keyboard' => [
+                        [
+                            ['text' => 'проверить артикулы'],
+                            ['text' => 'информация о боте'],
+                        ]
+                    ]
+                ]
+            ];
+            $send_data['chat_id'] = $data['chat']['id'];
+            $controller->sendTelegram($method, $send_data);
+    }
 }
 
